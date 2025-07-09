be3de214650260a3f1f7f5162a4ba8e513c2fa6d
nahul100 <205106nahuljulkernain@gmail.com>
2025-07-09 19:45:59 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React,{useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import { BrowserRouter,Routes,Route} from "react-router-dom";
+import Quiz from './Pages/Quiz';
+
+function App() {
+  return (   
+   <>
+     <Quiz />
+     
+   </>
+  );
+  
+}
+export default App
+
